5d56d7518a0c928ce5e662d0fd901533d42434fa
Shanmugapriya222 <shanmugapriyab211@gmail.com>
2026-01-12 14:43:24 +0530

    setup

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>🚗 Parking Management System</h1>
+      <p>Phase 1 setup is working successfully.</p>
+    </main>
+  );
+}
